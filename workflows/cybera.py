ccf5fd77a357dff6c70d8ccae191d0ea820f7ed2
Cameron Mann <camthemann@gmail.com>
2016-11-29 13:52:19 -0700

    Use custom template

--- workflows/cybera.py
+++ workflows/cybera.py
@@ -5,30 +5,7 @@ from horizon.workflows.base import Step
 
 
 class UpdateMembersStep(Step):
-    """A step that allows a user to add/remove members from a group.
-
-    .. attribute:: show_roles
-
-        Set to False to disable the display of the roles dropdown.
-
-    .. attribute:: available_list_title
-
-        The title used for the available list column.
-
-    .. attribute:: members_list_title
-
-        The title used for the members list column.
-
-    .. attribute:: no_available_text
-
-        The placeholder text used when the available list is empty.
-
-    .. attribute:: no_members_text
-
-        The placeholder text used when the members list is empty.
-
-    """
-    template_name = "horizon/common/_workflow_step_update_members.html"
+    template_name = "horizon/common/_workflow_step_update_members_cybera.html"
     show_roles = True
     available_list_title = _("All available")
     members_list_title = _("Members")
